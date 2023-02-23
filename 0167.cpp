fe8301c44fa73d6070fcfe8b6862c6c9b72da498
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2023-02-23 09:14:41 -0300

    Update 0167

---
+++ 0167.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> indices;
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum > target) right--; 
+            else if (sum < target) left++;
+            else break;
+        }
+        return { left + 1, right + 1 };
+    }
+};
