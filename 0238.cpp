83a3ad35c3626b945f2d573fba6006a647090fbd
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2023-03-21 07:14:40 -0300

    Update 0238

---
+++ 0238.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> answer(nums.size());
+        int prefix = 1, postfix = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            answer[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return answer;
+    }
+};
